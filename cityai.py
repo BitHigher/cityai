66d04c58cacb85cf3388fef54ea9bb4268c226ad
Zhu Hanfeng <zhuhanfeng@gmail.com>
2013-03-04 00:10:08 +0800

    rewrite

--- cityai.py
+++ cityai.py
@@ -1,136 +1,112 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
 from random import randint, random
-import numpy as np
 import matplotlib.pyplot as plt
-from math import sqrt
 import math
 
-def random_points(num):
-	points = []
-	for i in range(num):
-		points.append((randint(0, 1000), randint(0, 1000)))
-	
-	return points
+def random_point():
+	x = randint(0, 1000)
+	y = randint(0, 1000) 
+	return (x, y)
 
-def draw_points(points, clusters, centers, bestsol):
-	plt.figure(figsize=(20, 20))
+def random_points(n_points):
+	return [random_point() for i in xrange(n_points)]
 
-	for pindice in clusters:
-		plt.plot([points[i][0] for i in pindice], 
-				[points[i][1] for i in pindice], 'x--', ms=5.0);
+def distance(p1, p2):
+	return (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
 
-	plt.plot([p[0] for p in centers],
-			[p[1] for p in centers], 'bo', markersize=10.0);
+def kmeans(points, k):
+	centers = [random_point() for i in range(k)]
+	parts = [[] for i in range(k)]
 
+	looping = True
+	while looping:
+		for p in points:
+			best_c = 0
+			best_dist = distance(p, centers[0])
+			for i in range(1, len(centers)):
+				dist = distance(p, centers[i])
+				if dist < best_dist:
+					best_dist = dist
+					best_c = i
+			parts[best_c].append(p)
 
-	plt.plot([p[0] for p in bestsol],
-			[p[1] for p in bestsol], 'ro', markersize=10.0);
-	
-	plt.savefig('points.png')
-
-def distance(v1, v2):
-	length = min(len(v1), len(v2))
-	total = 0.0
-	for i in range(length):
-		total += (v1[i] - v2[i])**2
-	return sqrt(total)
-
-
-def kmeans(points, k, iterations=100):
-	# calculate the range of each dimension
-	ds = len(points[0])
-	ranges = [(min(p[i] for p in points), max(p[i] for p in points))
-				for i in range(ds)]
-	
-	centers = [[random()*(ranges[i][1]-ranges[i][0])+ranges[i][0] for i in range(ds)]
-				for j in range(k)]
-
-	while(iterations > 0):
-		iterations -= 1
-		clusters = [[] for i in range(k)]
-		newcenters = [[0 for i in range(ds)] for j in range(k)]
-		for i in range(len(points)):
-			minVal = distance(points[i], centers[0])
-			minIndex = 0
-			for j in range(1,k):
-				curVal = distance(points[i], centers[j])
-				if curVal < minVal:
-					minVal = curVal
-					minIndex = j
-			clusters[minIndex].append(i)
-			newcenters[minIndex] = [newcenters[minIndex][j] + points[i][j] for j in range(ds)]
-
+		# choose the new centers according parts
+		new_centers = []
 		for i in range(k):
-			length = float(len(clusters[i]))
-			if length == 0: length = 1.0
-			for j in range(ds):
-				newcenters[i][j] /= length
+			n = len(parts[i])
+			x = sum([p[0] for p in parts[i]])
+			y = sum([p[1] for p in parts[i]])
+			if n == 0:
+				new_centers.append(random_point())
+			else:
+				new_centers.append((x/float(n), y/float(n)))
 
-		if(newcenters == centers):
-			break
+		if centers == new_centers:
+			break;
 		else:
-			centers = newcenters
+			centers = new_centers
+			parts = [[] for i in range(k)]
+	return centers, parts
 
-	print iterations
-	return centers, clusters
+# ATTENTION!!! most important
+def costf(sol, parts):
+	cost = 0.
+	for c in sol:
+		for points in parts:
+			for p in points:
+				cost += distance(c, p)
+	return cost/10000000
 
-def cost(points, sol):
-	cost = 0.0
-	
-	for p in points:
-		for i in range(len(sol)/2):
-			cost += 1 - 1000/distance(p, (sol[2*i], sol[2*i+1]))
-	return cost
+def random_smooth_sol(current_sol):
+	sol = current_sol[:]
+	index = randint(0, len(current_sol)-1)
+	x = sol[index][0] + random()*20 - 10.0
+	y = sol[index][1] + random()*20 - 10.0
+	sol[index] = (x, y)
+	return sol
 
-def annealing(points, sol, costf=cost, step=5, T=10000.0, cool=0.95):
-	best = costf(points, sol)
-	bestsol = sol
+def anneal(parts, centers, T=10000.0, cool=0.95):	
+	best_sol = centers
+	best = costf(centers, parts)
 
-	anneal = []
-		
+	best_array = [best]
+	cost_array = [best]
 	while T > 0.1:
-		# random a index
-		index = randint(0, len(sol)-1)
-		# random a change
-		change = step
-		if(random() < 0.5):
-			change = -step
+		sol = random_smooth_sol(best_sol)
+		cost = costf(sol, parts)
+		if cost < best or random() < math.exp(-(cost-best)/T):
+			best = cost
+			best_sol = sol
 
-		bestsol[index] += change
-		curcost = costf(points, bestsol)
-        
-		if (curcost < best or random() < pow(math.e, -(curcost-best)/T)):
-			best = curcost
-		else:
-			bestsol[index] -= change
-
-		anneal.append((T, best, curcost))
+		best_array.append(best)
+		cost_array.append(cost)
 		T *= cool
 
-	return best, bestsol, anneal
-
-def main():
-	points = random_points(10000)
-	centers, clusters  = kmeans(points, 8)
-
-	sol = []
-	for p in centers:
-		sol.append(p[0])
-		sol.append(p[1])
+	return best_sol, best, best_array, cost_array
 
 
-	best, bestsol, anneal = annealing(points, sol)
-	bestsol = [(bestsol[2*i], bestsol[2*i+1]) for i in range(len(bestsol)/2)]
-	draw_points(points, clusters, centers, bestsol)
+def plot_points(parts, centers):
+	for points in parts:
+		plt.plot([p[0] for p in points], [p[1] for p in points], 'x')
+	
+	plt.plot([p[0] for p in centers], [p[1] for p in centers], 'ro')
+	return plt	
 
-#	print anneal
-	#draw anneal trend
-	plt.cla()
-	plt.figure(figsize=(20, 8))
-	plt.plot([i for i in range(len(anneal))],[p[2] for p in anneal],"gx-")
-	plt.plot([i for i in range(len(anneal))],[p[1] for p in anneal],"ro-")
-	plt.savefig('anneal.png')
+def test(n_points, k):
+	points = random_points(n_points)
+	centers, parts = kmeans(points, k)
+	sol, cost, best_array, cost_array = anneal(parts, centers)
 
-main()
+	plt = plot_points(parts, centers)
+	plt.plot([p[0] for p in sol], [p[1] for p in sol], 'go')
+	plt.savefig('anneal_result.png')
 
+	
+	x = [i for i in xrange(len(best_array))]
+	plt.clf()
+	plt.plot(x, cost_array, 'b')
+	plt.plot(x, best_array, 'r')
+	plt.savefig('annealing.png')
+
+
+if __name__ == '__main__':
+	test(10000, 5)
